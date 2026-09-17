35fd8040f4fde6720767cea3b7e76f812862ed19
shalinchristian <192890456+shalinchristian@users.noreply.github.com>
2026-09-17 23:07:08 +0530

    Initial commit

---
+++ resqora-backend/src/test/java/com/resqora/resqora_backend/ResqoraBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.resqora.resqora_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResqoraBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
